01405f5072cfecd1b6179475a7e9483b5e5f14ef
moatefdev <m.atef317@gmail.com>
2022-01-14 23:45:53 +0200

    Started react project

---
+++ client/gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require("gulp");
+const gulpSass = require("gulp-sass");
+const sass = gulpSass(require("sass"));
+
+gulp.task("sass", async function () {
+  gulp.src("src/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
+});
