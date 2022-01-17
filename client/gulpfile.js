721a6253fe01800d8efc1382b18c92648ffe8909
moatefdev <m.atef317@gmail.com>
2022-01-18 00:00:39 +0200

    footer comp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,3 +5,16 @@ const sass = gulpSass(require("sass"));
 gulp.task("sass", async function () {
   gulp.src("src/components/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
 });
+
+gulp.task("watch", async function () {
+  gulp.watch("src/components/**/*.scss", async function () {
+    gulp
+      .src("src/components/**/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("src/css"));
+  });
+});
+
+// gulp.task("watch", async function () {
+//   gulp.watch("src/components/**/*.scss", sass);
+// });
