129b9a8df3c5ca8aa6ce3c10d086a69079794eef
moatefdev <m.atef317@gmail.com>
2022-01-26 22:57:14 +0200

    server

---
+++ server/routes/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const Product = require("./../models/product");
+const router = express.Router();
+
+router.get("/api/products", async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
+
+router.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
+});
+
+router.delete("/api/products/:id", async (req, res) => {
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
+
+module.exports = router;
