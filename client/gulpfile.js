c1e4eeb9a8ba167878762c0dde4cccf984ef2d95
moatefdev <m.atef317@gmail.com>
2022-02-09 20:22:31 +0200

    add order page

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,17 +2,20 @@ const gulp = require("gulp");
 const gulpSass = require("gulp-sass");
 const sass = gulpSass(require("sass"));
 
-gulp.task("sass", async function () {
-  gulp.src("src/components/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
-});
+// gulp.task("sass", async function () {
+//   gulp.src("src/components/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
+// });
 
 gulp.task("watch", async function () {
-  gulp.watch("src/components/**/*.scss", async function () {
-    gulp
-      .src("src/components/**/*.scss")
-      .pipe(sass())
-      .pipe(gulp.dest("src/css"));
-  });
+  gulp.watch(
+    ["src/components/**/*.scss", "src/pages/*.scss"],
+    async function () {
+      gulp
+        .src(["src/components/**/*.scss", "src/pages/*.scss"])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"));
+    }
+  );
 });
 
 // gulp.task("watch", async function () {
--- client/src/pages/Order.js
+++ client/src/pages/Order.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchOrders } from "./../store/actions/orders";
+import "./../css/Order.css";
 
-function Order() {
-  return <div>Orders</div>;
+function Order(props) {
+  useEffect(() => {
+    props.fetchOrders();
+  }, []);
+
+  const { orders } = props;
+
+  return (
+    <div className="orders">
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>items</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders &&
+            orders.map((order) => {
+              return (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.name}</td>
+                  <td>{order.email}</td>
+                  <td>
+                    {props.cartItems.map((p) => (
+                      <p>
+                        {p.title} , qty: {p.qty}
+                      </p>
+                    ))}
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Order;
+export default connect(
+  (state) => {
+    return {
+      orders: state.order.orders,
+      cartItems: state.cart.cartItems,
+    };
+  },
+  { fetchOrders }
+)(Order);
