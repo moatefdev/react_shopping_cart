f06a53b5b3a2fda983cd5843f25abea2eb5ba592
moatefdev <m.atef317@gmail.com>
2022-02-10 18:16:47 +0200

    added configurations

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectionString = "mongodb://localhost/react-shopping-cart";
 function runDB() {
   mongoose
-    .connect(connectionString)
+    .connect(process.env.MONGO_URI || connectionString)
     .then((res) => console.log("Connection Done"));
 }
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
+require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const productRouter = require("./routes/product");
 const orderRouter = require("./routes/order");
@@ -8,12 +8,21 @@ const runDB = require("./config/db");
 const app = express();
 
 app.use(bodyParser.json());
-app.use("/", productRouter);
-app.use("/", orderRouter);
 
 // Run DB
 runDB();
 
-app.listen(5001, () => {
+app.use("/", productRouter);
+app.use("/", orderRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("public"));
+  app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
+} else {
+  app.get("/", (req, res) => res.send("API Running"));
+}
+
+const PORT = process.env.PORT;
+app.listen(PORT || 5001, () => {
   console.log("Running on port 5001");
 });
